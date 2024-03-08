524845652ef64346188abfa1652462c2e7ec73a5
akgunkaya <akgunkaya12@gmail.com>
2024-03-08 23:23:56 +0000

    added sidebar and added darkmode

---
+++ taskapp/context_processors.py
@@ -0,0 +1,24 @@
+
+def global_data(request):
+    # Define the navigation items
+    nav_items = [
+        {'name': 'Home', 'url_name': 'home', 'icon': 'home'},
+        {'name': 'Login', 'url_name': 'login', 'icon': 'login'},
+        {'name': 'Sign Up', 'url_name': 'signup', 'icon': 'passkey'},
+        {'name': 'Tasks', 'url_name': 'tasks', 'icon': 'task'},
+        {'name': 'Projects', 'url_name': 'projects', 'icon': 'view_list'},
+        {'name': 'Organizations', 'url_name': 'organizations', 'icon': 'corporate_fare'},
+        {'name': 'Notifications', 'url_name': 'notifications', 'icon': 'notifications'},
+        {'name': 'Log out', 'url_name': 'logout', 'icon': 'logout'}
+    ]
+
+
+    # Check user authentication and adjust nav_items accordingly
+    if request.user.is_authenticated:
+        # Filter out 'Login' and 'Sign Up' for authenticated users
+        nav_items = [item for item in nav_items if item['name'] not in ['Login', 'Sign Up']]
+    else:
+        # Filter out items not relevant for unauthenticated users
+        nav_items = [item for item in nav_items if item['name'] in ['Home', 'Login', 'Sign Up']]
+
+    return {'nav_items': nav_items}
